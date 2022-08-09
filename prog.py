e3637d6991e4d365dd4e19084dfbe95cb410174b
Artem Kovalev <rrarrk123@gmail.com>
2022-08-09 19:00:01 +0300

    Графический рисунок множества квадратов

---
+++ prog.py
@@ -0,0 +1,20 @@
+import turtle
+
+def sq(a):
+    for i in range(4):
+        joe.forward(a)
+        joe.left(90)
+
+colors = ['red','brown','green','blue']
+
+joe = turtle.Turtle()
+joe.speed(10)#скорость крафического рисования
+joe.color('red')
+
+dlina =40
+for i in range(36):
+    joe.color(colors[i%4])
+    sq(dlina)
+    joe.right(10)
+    dlina+=5
+
